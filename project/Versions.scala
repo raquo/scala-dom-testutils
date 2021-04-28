f2b6cc504ea3be1bccb2fc43089effa694a1e382
Nikita Gazarov <nikita@raquo.com>
2021-04-27 17:13:26 -0700

    Build: Scala 3.0.0-RC3

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,9 +6,11 @@ object Versions {
 
   val Scala_3_RC2 = "3.0.0-RC2"
 
-  val ScalaDomTypes = "0.14.2"
+  val Scala_3_RC3 = "3.0.0-RC3"
 
-  val ScalaTest = "3.2.7"
+  val ScalaDomTypes = "0.14.3"
+
+  val ScalaTest = "3.2.8"
 
   val JsDom = "16.4.0"
 }
